cf598737028cdcb039fa116e45d11bf40bc02153
PinkDude <34643770+PinkDude@users.noreply.github.com>
2018-06-29 21:47:29 +0300

    Initial commit

---
+++ ChatBot/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChatBot.Models;
+using DALforChatBot.Repositories;
+using ModelEntities.Models;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace ChatBot.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public static List<Jsons> listJsons = new List<Jsons>();
+        public static ListJ listJ = new ListJ();
+        ChatBotUnitOfWork _unit;
+
+        public HomeController(ChatBotUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public IActionResult RefUsers()
+        {
+            return PartialView("Users", listJ);
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxTest(Jsons jo)
+        {
+            if (jo.GetProv())
+            {
+                listJ.listJ.Add(jo);
+            }
+            else
+            {
+                jo.GoToBD(_unit);
+            }
+            return Json(listJ.listJ);
+        }
+
+        [HttpPost]
+        public JsonResult GetAnswer(Jsons jo)
+        {
+            //отправка в БД и МЛ
+            foreach(Jsons j in listJ.listJ)
+            {
+                if(j.Number == jo.Number)
+                {
+                    listJ.listJ.Remove(j);
+                    break;
+                }
+            }
+            jo.GoToBD(_unit);
+            return Json(jo);
+        }
+        
+        public IActionResult Index()
+        {
+            return View(listJ); 
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
---
+++ ChatBot/Models/Jsons.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DALforChatBot.Repositories;
+using ModelEntities.Models;
+
+namespace ChatBot.Models
+{
+    public class ListJ
+    {
+        public List<Jsons> listJ { get; set; }
+
+        public ListJ()
+        {
+            listJ = new List<Jsons>();
+        }
+    }
+
+    public class Jsons
+    {
+        public string Number { get; set; }
+        public string Request { get; set; }
+        public string[][] Response { get; set; } = new string[3][] {new string[2], new string[2], new string[2] };
+        //public string[] Res { get; set; }// = new string[3];
+        public string Operator { get; set; }
+        public string OperatorVar { get; set; }
+
+        //public string[][] pom { get; set; }
+
+        public bool GetProv()
+        {
+            //try
+            //{
+                if (Response[0][1] == string.Empty)
+                {
+                    //отправка оператору
+                    return true;
+                }
+                else
+                {
+                    if (Convert.ToInt32(Response[0][1]) > 90)
+                    {
+                        //просто сохранение в БД
+                        return false;
+                    }
+                    else
+                    {
+                        //отправка оператору
+                        return true;
+                    }
+                }
+            /*}
+            catch(Exception ex)
+            {
+                throw ex;
+            }*/
+        }
+
+        public async void GoToBD(ChatBotUnitOfWork ChatBot)
+        {          
+            User user = ChatBot.CommonUsersRepository.FindByPhone(Number).Result;
+
+            if(user == null)
+            {
+                user = new User(Number);
+                await ChatBot.CommonUsersRepository.Create(user);
+            }
+
+            if(OperatorVar == null)
+            {
+                OperatorVar = Response[0][0];
+            }
+
+            Operator oper = new Operator("Kek", "Cheburek", "Login", "Password");
+
+            MessageInfo MesInfo = new MessageInfo();
+            MesInfo.User = user;
+            MesInfo.Operator = oper;
+            MesInfo.Message = Request;
+            MesInfo.Date = DateTime.Now;
+            MesInfo.OperatorsVar = OperatorVar;
+            MesInfo.Agility = 0.5f;
+
+            MesInfo.VarOne = Response[0][0] + " : " + Response[0][1];
+            MesInfo.VarTwo = Response[1][0] + " : " + Response[1][1];
+            MesInfo.VarThree = Response[2][0] + " : " + Response[2][1];
+
+            await ChatBot.MessagesRepository.Create(MesInfo);
+
+            await ChatBot.SaveAsync();
+        }
+    }
+}
