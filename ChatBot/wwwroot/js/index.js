7ad2c2988159e1aa705aa58807c2cfad02beb82f
PinkDude <34643770+PinkDude@users.noreply.github.com>
2018-06-30 15:12:12 +0300

    Add files via upload

---
+++ ChatBot/wwwroot/js/index.js
@@ -0,0 +1,37 @@
+﻿
+
+function goMessage(id) {
+    var x = document.getElementById(id).innerHTML;
+    var jo = {
+        Number: document.getElementById('topbar').innerHTML,
+        Request: document.getElementById('messages').innerHTML,
+        Response: [[document.getElementById('label1').innerHTML, document.getElementById('agil1').innerHTML], [document.getElementById('label2').innerHTML, document.getElementById('agil2').innerHTML], [document.getElementById('label3').innerHTML, document.getElementById('agil3').innerHTML]],
+        OperatorVar: x
+    }
+    //TODO : доделай чтоб он помечался только после успешной отправки
+    var nowLab = document.getElementById('nowLab').innerHTML;
+    document.getElementById(nowLab).style.background = 'brown';
+
+    document.getElementById('topbar').innerHTML = "";
+    document.getElementById('messages').innerHTML = "";
+    document.getElementById('label1').innerHTML = "";
+    document.getElementById('label2').innerHTML = "";
+    document.getElementById('label3').innerHTML = "";
+
+    $.ajax({
+        type: "POST",
+        data: { jo },
+        url: "/Home/GetAnswer",
+        accept: 'application / json',
+        success: function (data) {
+        },
+        error: function (result) {
+            alert("Something Went Wrong");
+
+        }
+
+    })
+    document.getElementById('label1').style.visibility = "hidden";
+    document.getElementById('label2').style.visibility = "hidden";
+    document.getElementById('label3').style.visibility = "hidden";
+}
---
+++ ChatBot/wwwroot/js/users.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    addIds('Users', 'label', 'lab');
+    addIds('Users', 'input', 'inplab');
+})
+
+function addIds(container, elName, prefix) {
+    var container = document.getElementById(container);
+    var tables = container.getElementsByTagName(elName);
+
+    for (var i = 0; i < tables.length; i++) {
+        tables[i].id = prefix + (i + 1);
+
+    }
+
+}
+
+function Func1(id = "Выберите пользователя", mes, a1, a2, a3, agl1, agl2, agl3, cl) {
+    if (document.getElementById(cl).style.background != 'brown') {
+        document.getElementById(cl).style.color = 'blue';
+        document.getElementById("nowLab").innerHTML = cl;
+        document.getElementById("topbar").innerHTML = id;
+        document.getElementById("messages").innerHTML = mes;
+        document.getElementById("label1").innerHTML = a1;
+        document.getElementById("label2").innerHTML = a2;
+        document.getElementById("label3").innerHTML = a3;
+        document.getElementById('label1').style.visibility = "visible";
+        document.getElementById('label2').style.visibility = "visible";
+        document.getElementById('label3').style.visibility = "visible";
+        document.getElementById("agil1").innerHTML = agl1;
+        document.getElementById("agil2").innerHTML = agl2;
+        document.getElementById("agil3").innerHTML = agl3;
+        document.getElementById("nowLab").innerHTML = cl;
+    }
+}
